7f12bac3d60ef489df33aaa8886373105588c5ba
Mateus Pereira <mateus.pereira4@gmail.com>
2022-08-23 22:04:25 -0300

    inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  altura : string
+  peso : string
+  opcao : string
+  resultado : string
+  mensagem : string
+  imagem : any
 
   constructor() {}
+
+  calcIMC(){
+    
+    var altura = parseFloat(this.altura)
+    var peso = parseFloat(this.peso)
+    var opcao = this.opcao
+    var resultado = parseFloat(this.resultado)
+
+    if(opcao == "f"){
+      resultado = peso / altura**2
+      this.resultado = resultado.toFixed(1)
+      if(resultado <= 19){
+        this.mensagem = "ABAIXO DO PESO"
+        this.imagem = ""        
+      }
+      else if (resultado > 19 && resultado <= 27.3){
+        this.mensagem = "PESO NORMAL"
+        this.imagem = ""
+      }
+      else{
+        this.mensagem = "ACIMA DO PESO"
+        this.imagem = "assets/alerta.png"
+      }
+
+    }
+    else if (opcao == "m"){
+      resultado = peso / altura**2
+      this.resultado = resultado.toFixed(1)
+      if(resultado <=20.7){
+        this.mensagem = "ABAIXO DO PESO"
+        this.imagem = ""
+      }
+      else if(resultado > 20.7 && resultado <=27.8){
+        this.mensagem = "PESO NORMAL"
+        this.imagem = ""
+      }
+      else{
+        this.mensagem = "ACIMA DO PESO"
+        this.imagem = "assets/alerta.png"
+      }
+      
+    }
+    else if(opcao == null) {
+      this.mensagem = "Escolha o sexo"
+      this.imagem = ""
+    }
+  }
 
 }
